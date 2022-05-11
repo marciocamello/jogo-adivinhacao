21ebf8f1301a53655fa7ad0a34a3da3381de1e1f
mvale <mvale@smardigit.pt>
2022-05-11 18:33:21 +0100

    first commit

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "***************************************";
+    std::cout << "* Boas vindas ao Jogo da Adivinhação! *";
+    std::cout << "***************************************";
+}
